c83b4e7daa5342e683e6f6dad49c559ef9fe8872
Fran García <fgarciadevelop@gmail.com>
2024-07-29 12:12:18 +0200

    feat: button back to asamblea

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {}
+export class FooterComponent {
+
+  constructor(
+    private cookieService: CookieService
+  ) {}
+
+  redireccionar() {
+    if (Boolean(this.cookieService.get('href'))) {
+      window.location.href = this.cookieService.get('href');
+    } else {
+      window.location.href = 'https://plenos.hogueras.es';
+    }
+  }
+}
