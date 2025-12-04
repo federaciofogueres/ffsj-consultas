d82090baa975398ce690e219c99c22789d5d26b6
Fran García <fgarciadevelop@gmail.com>
2025-12-04 19:37:48 +0100

    fix: fix asistencias change

--- src/app/components/consultas/consultas.component.ts
+++ src/app/components/consultas/consultas.component.ts
@@ -6,70 +6,72 @@ import { AutorizacionesConsultasService, Consulta, ConsultasService } from "../.
 import { ConsultasExtraService } from "../../services/consultas-extra.service";
 
 @Component({
-    selector: 'app-consultas',
-    standalone: true,
-    imports: [
-      FfsjSpinnerComponent
-    ],
-    templateUrl: './consultas.component.html',
-    styleUrl: './consultas.component.scss'
-  })
-  export class ConsultasComponent {
+  selector: 'app-consultas',
+  standalone: true,
+  imports: [
+    FfsjSpinnerComponent
+  ],
+  templateUrl: './consultas.component.html',
+  styleUrl: './consultas.component.scss'
+})
+export class ConsultasComponent {
 
-    consultas: Consulta[] = [];
-    loading: boolean = false;
-    idAsociado: number = -1;
+  consultas: Consulta[] = [];
+  loading: boolean = false;
+  idAsociado: number = -1;
+  idAsistencias: number[] = [];
 
-    constructor(
-      private consultasService: ConsultasService,
-      private autorizacionesConsultasService: AutorizacionesConsultasService,
-      private router: Router,
-      private consultasExtraService: ConsultasExtraService
-    ) {}
+  constructor(
+    private consultasService: ConsultasService,
+    private autorizacionesConsultasService: AutorizacionesConsultasService,
+    private router: Router,
+    private consultasExtraService: ConsultasExtraService
+  ) { }
 
-    ngOnInit() {
-      this.loading = true;
-      this.idAsociado = this.consultasExtraService.getIdUsuario();
-      this.loadConsultas();
-    }
-
-    async isUserAuthorized(idConsulta: number) {
-      try {
-        const response: any = await firstValueFrom(
-          this.autorizacionesConsultasService.consultasIdConsultaAutorizadosIdAsistenciaGet(idConsulta, this.idAsociado)
-        );
-        console.log(response);
-        return response.status.status === 200 && response.autorizaciones !== 0;
-      } catch (error) {
-        console.error(error);
-        return false;
-      }
-    }
-
-    async loadConsultas() {
-      try {
-        const response = await firstValueFrom(this.consultasService.consultasGet());
-        console.log(response);
-        if (response.status.status === 200) {
-          const autorizaciones = await Promise.all(
-            response.consultas.map(async consulta => ({
-              consulta,
-              autorizado: await this.isUserAuthorized(consulta.id)
-            }))
-          );
-          this.consultas = autorizaciones
-            .filter(({ autorizado }) => autorizado)
-            .map(({ consulta }) => consulta);
-        }
-      } catch (error) {
-        console.error(error);
-      } finally {
-        this.loading = false;
-      }
-    }
-
-    loadConsulta(consulta: Consulta) {
-      this.router.navigate(['/consultas', consulta.id]);
-    }
-
+  ngOnInit() {
+    this.loading = true;
+    this.idAsistencias = this.consultasExtraService.getAsistencias();
+    this.idAsociado = this.consultasExtraService.getIdUsuario();
+    this.loadConsultas();
   }
+
+  async isUserAuthorized(idConsulta: number) {
+    try {
+      const response: any = await firstValueFrom(
+        this.autorizacionesConsultasService.consultasIdConsultaAutorizadosIdAsistenciaGet(idConsulta, this.idAsistencias[0])
+      );
+      console.log(response);
+      return response.status.status === 200 && response.autorizaciones !== 0;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+  async loadConsultas() {
+    try {
+      const response = await firstValueFrom(this.consultasService.consultasGet());
+      console.log(response);
+      if (response.status.status === 200) {
+        const autorizaciones = await Promise.all(
+          response.consultas.map(async consulta => ({
+            consulta,
+            autorizado: await this.isUserAuthorized(consulta.id)
+          }))
+        );
+        this.consultas = autorizaciones
+          .filter(({ autorizado }) => autorizado)
+          .map(({ consulta }) => consulta);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  loadConsulta(consulta: Consulta) {
+    this.router.navigate(['/consultas', consulta.id]);
+  }
+
+}
--- src/app/services/consultas-extra.service.ts
+++ src/app/services/consultas-extra.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from "@angular/core";
 import { AuthService } from "ffsj-web-components";
+import { CookieService } from "ngx-cookie-service";
 
 @Injectable({
     providedIn: 'root'
-  })
+})
 export class ConsultasExtraService {
 
     private idUsuario = -1;
+    private asistenciasUsuario: number[] = [];
 
     constructor(
-        private authService: AuthService
-    ){
+        private authService: AuthService,
+        private cookieService: CookieService
+    ) {
         this.loadPlenoFromCookies();
     }
 
@@ -20,6 +23,11 @@ export class ConsultasExtraService {
 
     getIdUsuario() {
         return this.idUsuario;
+    }
+
+    getAsistencias() {
+        const asistencias = this.cookieService.get('asistencias');
+        return this.asistenciasUsuario.length > 0 ? this.asistenciasUsuario : (asistencias ? JSON.parse(asistencias) : []);
     }
 
 }
